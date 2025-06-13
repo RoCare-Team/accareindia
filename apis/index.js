b8147fd9634a1ca7ad848453903322ceb695bfb6
Sachi8565 <101034229+Sachi8565@users.noreply.github.com>
2025-06-13 14:38:09 +0530

    firebase account key

--- apis/index.js
+++ apis/index.js
@@ -4,11 +4,18 @@ const admin = require('firebase-admin');
 const app = express();
 const port = 5000;
 
-const serviceAccount = require('./accare_firebase.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) { // Prevent re-initialization in dev
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+    }),
+  });
+}
 
 const db = admin.firestore();
 const collection = 'items';
